0f19f0833c8b9226c0a41fd2c3d9166fa417a679
xtr4nge <xtr4nge@gmail.com>
2014-02-26 23:30:39 +0000

    minor fix

--- includes/FruityWifi-autostart.php
+++ includes/FruityWifi-autostart.php
@@ -18,6 +18,7 @@ $login_url = "$protocol://localhost$web_path/login.php";
 
 $ch = curl_init();
 
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_USERAGENT, $agent);
 curl_setopt($ch, CURLOPT_URL, $login_url );
